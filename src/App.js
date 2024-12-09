6031561d522b66b9166c3ec73d328f1fd81af5c9
Emma <n.ba.04@mail.ru@>
2024-12-09 19:57:21 +0500

    сделала регистрацию

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage.jsx'
+import RegPage from './pages/RegPage.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="login" element={<LoginPage />} />
+        <Route path="registration" element={<RegPage />} />
       </Routes>
     </BrowserRouter>
   );
