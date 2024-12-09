4d725f37670b37f3b0e1d461211900ff881f3565
Emma <n.ba.04@mail.ru@>
2024-12-09 19:44:07 +0500

    сделана верстка основных компонентов и страницы входа

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import ALink from "./ALink";
+
+export default function Header() {
+    return (
+        <header className='Header'>
+            <ALink Text="каталог тестовых заданий"></ALink>
+            <ALink Text="каталог стажировок"></ALink>
+        </header>
+    )
+}
