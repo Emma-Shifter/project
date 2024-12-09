fe0f0f433e87b714574c3865cd83e26234573572
Emma <n.ba.04@mail.ru@>
2024-12-09 20:16:30 +0500

    сделана страница восстановления пароля

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage.jsx'
 import RegPage from './pages/RegPage.jsx';
+import RestorePasswordPage from './pages/RestorePasswordPage.jsx';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="login" element={<LoginPage />} />
         <Route path="registration" element={<RegPage />} />
+        <Route path="restore" element={<RestorePasswordPage />} />
       </Routes>
     </BrowserRouter>
   );
