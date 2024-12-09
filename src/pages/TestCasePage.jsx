71477dc7077add91a975b8b94b694ee094e021db
Emma <n.ba.04@mail.ru@>
2024-12-10 00:45:26 +0500

    сделана верстка тестового

--- src/pages/TestCasePage.jsx
+++ src/pages/TestCasePage.jsx
@@ -1,4 +1,8 @@
 import Header from '../components/Header';
+import star from '../img/star.svg';
+import peopleApproveIcon from '../img/people-approve.svg';
+import Button from '../components/Button';
+import nextIcon from '../img/next.svg';
 
 export default function TestCasePage() {
     return (
@@ -6,8 +10,19 @@ export default function TestCasePage() {
             <Header></Header>
             <p className="main-title">НАЗВАНИЕ <br></br>ТЕСТОВОГО ЗАДАНИЯ</p>
             <div className='testcase-container'>
-                <div></div>
-                
+                <div className='testcase-info-container'>
+                    <div className='testcase-info'>
+                        <img alt="people-approve" src={peopleApproveIcon}></img>
+                        <p>15</p>
+                    </div>
+                    <img alt="star" src={star}></img>
+                </div>
+                <textarea rows={7}>текстекстекстексттекстекстекстексттекстекстекстексттекстекстекстексттекстекстекстексттекстекстекстексттекстекстекстексттекстекстекстексттекстекстекстексттекстекстекстексттекстекстекстексттекстекстекстексттекстекстекстексттекстекстекстекст</textarea>
+                <Button buttonText="ПОСМОТРЕТЬ ОТВЕТ"></Button>
+                <div className='similar'>
+                    <img alt="next" src={nextIcon}></img>
+                    <p>ПОХОЖЕЕ</p>
+                </div>
             </div>
         </div>
     )
